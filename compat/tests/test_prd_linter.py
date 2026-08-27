ee49cb1f4d19542342e00b00d296d334ed9cb328
Yonghye Kwon <developer.0hye@gmail.com>
2026-08-27 17:53:48 +0700

    docs: record DX-012 evidence
    Signed-off-by: Yonghye Kwon <developer.0hye@gmail.com>

--- compat/tests/test_prd_linter.py
+++ compat/tests/test_prd_linter.py
@@ -166,7 +166,7 @@ class PrdLinterContractTests(unittest.TestCase):
         )
 
         self.assertGreater(result.task_count, 700)
-        self.assertEqual(result.checked_count, 31)
+        self.assertEqual(result.checked_count, 32)
         self.assertGreaterEqual(result.evidence_count, result.checked_count)
 
 
