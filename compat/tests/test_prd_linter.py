bd8fd339d7103979be0692058991e88173a8c659
Yonghye Kwon <developer.0hye@gmail.com>
2026-08-28 17:17:52 +0700

    test: advance PRD completion count
    Signed-off-by: Yonghye Kwon <developer.0hye@gmail.com>

--- compat/tests/test_prd_linter.py
+++ compat/tests/test_prd_linter.py
@@ -166,7 +166,7 @@ class PrdLinterContractTests(unittest.TestCase):
         )
 
         self.assertGreater(result.task_count, 700)
-        self.assertEqual(result.checked_count, 47)
+        self.assertEqual(result.checked_count, 48)
         self.assertGreaterEqual(result.evidence_count, result.checked_count)
 
 
