22aa02f7b634a75b2dba10b15ee0693ae6419d43
Yonghye Kwon <developer.0hye@gmail.com>
2026-08-29 18:35:59 +0700

    test(prd): advance checked task contracts
    Signed-off-by: Yonghye Kwon <developer.0hye@gmail.com>

--- compat/tests/test_prd_linter.py
+++ compat/tests/test_prd_linter.py
@@ -166,7 +166,7 @@ class PrdLinterContractTests(unittest.TestCase):
         )
 
         self.assertGreater(result.task_count, 700)
-        self.assertEqual(result.checked_count, 69)
+        self.assertEqual(result.checked_count, 70)
         self.assertGreaterEqual(result.evidence_count, result.checked_count)
 
 
