d781e220c783ea442a28336cc9e898e57e9a8da1
Yonghye Kwon <developer.0hye@gmail.com>
2026-08-30 06:38:38 +0700

    docs: record DOC-012 remote evidence
    Signed-off-by: Yonghye Kwon <developer.0hye@gmail.com>

--- compat/tests/test_prd_linter.py
+++ compat/tests/test_prd_linter.py
@@ -166,7 +166,7 @@ class PrdLinterContractTests(unittest.TestCase):
         )
 
         self.assertGreater(result.task_count, 700)
-        self.assertEqual(result.checked_count, 81)
+        self.assertEqual(result.checked_count, 82)
         self.assertGreaterEqual(result.evidence_count, result.checked_count)
 
 
